4778648930dd68cb5d5ee933f16226a087c5493e
hatt0519 <moroku0519@gmail.com>
2024-07-16 18:34:49 +0900

    remove filter

--- lib/flutter_analyze_parser.rb
+++ lib/flutter_analyze_parser.rb
@@ -8,7 +8,6 @@ class FlutterAnalyzeParser
       return [] if filtered_input.detect { |element| element.include? "No issues found!" }
 
       filtered_input
-        .select { |line| line.start_with? "info" }
         .map(&method(:parse_line))
     end
 
